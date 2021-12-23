c8d64d262f096a154ba488b7eb9ea93e73c5bbbe
sammyalwayscode <olorundasamuel2@gmail.com>
2021-12-23 14:36:27 +0100

    new changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,165 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
   return (
-    <div>
-      Mobile app development is the act or process by which a mobile app is
-      developed for mobile devices, such as personal digital assistants,
-      enterprise digital assistants or mobile phones. These applications can be
-      pre-installed on phones during manufacturing platforms, or delivered as
-      web applications using server-side or client-side processing (e.g.,
-      JavaScript) to provide an "application-like" experience within a Web
-      browser. Application software developers also must consider a long array
-      of screen sizes, hardware specifications, and configurations because of
-      intense competition in mobile software and changes within each of the
-      platforms. Mobile app development has been steadily growing, in revenues
-      and jobs created. A 2013 analyst report estimates there are 529,000 direct
-      app economy jobs within the EU then 28 members (including the UK), 60
-      percent of which are mobile app developers.[1] As part of the development
-      process, mobile user interface (UI) design is also essential in the
-      creation of mobile apps. Mobile UI considers constraints, contexts,
-      screen, input, and mobility as outlines for design. The user is often the
-      focus of interaction with their device, and the interface entails
-      components of both hardware and software. User input allows for the users
-      to manipulate a system, and device's output allows the system to indicate
-      the effects of the users' manipulation. Mobile UI design constraints
-      include limited attention and form factors, such as a mobile device's
-      screen size for a user's hand(s). Mobile UI contexts signal cues from user
-      activity, such as location and scheduling that can be shown from user
-      interactions within a mobile app. Overall, mobile UI design's goal is
-      mainly for an understandable, user-friendly interface. The UI of mobile
-      apps should: consider users' limited attention, minimize keystrokes, and
-      be task-oriented with a minimum set of functions. This functionality is
-      supported by mobile enterprise application platforms or integrated
-      development environments (IDEs). Mobile UIs, or front-ends, rely on mobile
-      back-ends to support access to enterprise systems. The mobile back-end
-      facilitates data routing, security, authentication, authorization, working
-      off-line, and service orchestration. This functionality is supported by a
-      mix of middleware components including mobile app server, mobile backend
-      as a service (MBaaS), and service-oriented architecture (SOA)
-      infrastructure. Mobile app development is becoming more critical for many
-      businesses with more than 3 billion people worldwide using smartphones,
-      more than 1.5 billion using tablets as of 2019. Users, on average, spend
-      90 percent of their mobile time in apps and there are more than 700
-      million apps downloads from various app stores.[2]
-      .............................................................................................................................
-      Web development is the work involved in developing a Web site for the
-      Internet (World Wide Web) or an intranet (a private network).[1] Web
-      development can range from developing a simple single static page of plain
-      text to complex Web-based Internet applications (Web apps), electronic
-      businesses, and social network services. A more comprehensive list of
-      tasks to which Web development commonly refers, may include Web
-      engineering, Web design, Web content development, client liaison,
-      client-side/server-side scripting, Web server and network security
-      configuration, and e-commerce development. Among Web professionals, "Web
-      development" usually refers to the main non-design aspects of building Web
-      sites: writing markup and coding.[2] Web development may use content
-      management systems (CMS) to make content changes easier and available with
-      basic technical skills. For larger organizations and businesses, Web
-      development teams can consist of hundreds of people (Web developers) and
-      follow standard methods like Agile methodologies while developing Web
-      sites. Smaller organizations may only require a single permanent or
-      contracting developer, or secondary assignment to related job positions
-      such as a graphic designer or information systems technician. Web
-      development may be a collaborative effort between departments rather than
-      the domain of a designated department. There are three kinds of Web
-      developer specialization: front-end developer, back-end developer, and
-      full-stack developer. Front-end developers are responsible for behavior
-      and visuals that run in the user browser, while back-end developers deal
-      with the servers.
-      .....................................................................................................................
-      Artificial intelligence (AI), is intelligence demonstrated by machines,
-      which is unlike the natural intelligence displayed by humans and animal.
-      Leading AI literature defines the field as the study of "intelligent
-      agents": any device that perceives its environment and takes actions that
-      maximize its chance of successfully achieving its goals.[3] Colloquially,
-      the term "artificial intelligence" is often used to describe machines (or
-      computers) that mimic "cognitive" functions that humans associate with the
-      human mind, such as "learning" and "problem solving".[4] Investopedia
-      defines AI as "the simulation of human intelligence in machines that are
-      programmed to think like humans and mimic their actions."[5] As machines
-      become increasingly capable, tasks considered to require "intelligence"
-      are often removed from the definition of AI, a phenomenon known as the AI
-      effect.[6] A quip in Tesler's Theorem says "AI is whatever hasn't been
-      done yet."[7] For instance, optical character recognition is frequently
-      excluded from things considered to be AI,[8] having become a routine
-      technology.[9] Modern machine capabilities generally classified as AI
-      include successfully understanding human speech,[10] competing at the
-      highest level in strategic game systems (such as chess and Go),[11]
-      autonomously operating cars, intelligent routing in content delivery
-      networks, and military simulations.[12] Artificial intelligence was
-      founded as an academic discipline in 1955, and in the years since has
-      experienced several waves of optimism,[13][14] followed by disappointment
-      and the loss of funding (known as an "AI winter"),[15][16] followed by new
-      approaches, success and renewed funding.[14][17] After AlphaGo
-      successfully defeated a professional Go player in 2015, artificial
-      intelligence once again attracted widespread global attention.[18] For
-      most of its history, AI research has been divided into sub-fields that
-      often fail to communicate with each other.[19] These sub-fields are based
-      on technical considerations, such as particular goals (e.g. "robotics" or
-      "machine learning"),[20] the use of particular tools ("logic" or
-      artificial neural networks), or deep philosophical
-      differences.[23][24][25] Sub-fields have also been based on social factors
-      (particular institutions or the work of particular researchers).[19] The
-      traditional problems (or goals) of AI research include reasoning,
-      knowledge representation, planning, learning, natural language processing,
-      perception and the ability to move and manipulate objects.[20] General
-      intelligence is among the field's long-term goals.[26] Approaches include
-      statistical methods, computational intelligence, and traditional symbolic
-      AI. Many tools are used in AI, including versions of search and
-      mathematical optimization, artificial neural networks, and methods based
-      on statistics, probability and economics. The AI field draws upon computer
-      science, information engineering, mathematics, psychology, linguistics,
-      philosophy, and many other fields. The field was founded on the assumption
-      that human intelligence "can be so precisely described that a machine can
-      be made to simulate it".[27] This raises philosophical arguments about the
-      mind and the ethics of creating artificial beings endowed with human-like
-      intelligence. These issues have been explored by myth, fiction and
-      philosophy since antiquity.[32] Some people also consider AI to be a
-      danger to humanity if it progresses unabated.[33][34] Others believe that
-      AI, unlike previous technological revolutions, will create a risk of mass
-      unemployment.[35]
-      .........................................................................................................................
-      Machine learning (ML) is the study of computer algorithms that improve
-      automatically through experience.[1] It is seen as a subset of artificial
-      intelligence. Machine learning algorithms build a model based on sample
-      data, known as "training data", in order to make predictions or decisions
-      without being explicitly programmed to do so.[2] Machine learning
-      algorithms are used in a wide variety of applications, such as email
-      filtering and computer vision, where it is difficult or unfeasible to
-      develop conventional algorithms to perform the needed tasks. A subset of
-      machine learning is closely related to computational statistics, which
-      focuses on making predictions using computers; but not all machine
-      learning is statistical learning. The study of mathematical optimization
-      delivers methods, theory and application domains to the field of machine
-      learning. Data mining is a related field of study, focusing on exploratory
-      data analysis through unsupervised learning.[4][5] In its application
-      across business problems, machine learning is also referred to as
-      predictive analytics.
-      ...........................................................................................................
-      User interface design (UI) or user interface engineering is the design of
-      user interfaces for machines and software, such as computers, home
-      appliances, mobile devices, and other electronic devices, with the focus
-      on maximizing usability and the user experience. The goal of user
-      interface design is to make the user's interaction as simple and efficient
-      as possible, in terms of accomplishing user goals (user-centered design).
-      Good user interface design facilitates finishing the task at hand without
-      drawing unnecessary attention to itself. Graphic design and typography are
-      utilized to support its usability, influencing how the user performs
-      certain interactions and improving the aesthetic appeal of the design;
-      design aesthetics may enhance or detract from the ability of users to use
-      the functions of the interface.[1] The design process must balance
-      technical functionality and visual elements (e.g., mental model) to create
-      a system that is not only operational but also usable and adaptable to
-      changing user needs. Interface design is involved in a wide range of
-      projects, from computer systems, to cars, to commercial planes; all of
-      these projects involve much of the same basic human interactions yet also
-      require some unique skills and knowledge. As a result, designers tend to
-      specialize in certain types of projects and have skills centered on their
-      expertise, whether it is a software design, user research, web design, or
-      industrial design.
-    </div>
+    <Container>
+      <Wrapper>
+        <ImageDiv> Image</ImageDiv>
+        <MeDescribe> This is me</MeDescribe>
+      </Wrapper>
+    </Container>
   );
 };
 
 export default Home;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  min-height: 100vh;
+`;
+const Wrapper = styled.div`
+  width: 85%;
+  background-color: red;
+  display: flex;
+  justify-content: space-between;
+`;
+const ImageDiv = styled.div``;
+const MeDescribe = styled.div``;
---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Skills = () => {
+  return <div>This is the skills</div>;
+};
+
+export default Skills;
